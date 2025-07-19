574a7f0c081b396308a29c953599f3d445d98fb1
Dillon Carey <dillon.carey030@gmail.com>
2025-07-18 18:17:27 -0700

    title

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Icebreaker",
+  title: "Windmatrix",
   description: "Next-Generation Neuronic Intelligence Interface",
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,12 @@ export default function Home() {
     <div className=''>
       <div className="text-center flex flex-row justify-center gap-20 py-10 bg-gray-900">
         <Link href='/bergs'><p className='text-white pt-1 text-lg'>Bergs</p></Link>
-        <p className='text-blue-200 text-2xl'>Icebreaker</p>
+        <p className='text-blue-200 text-2xl'>Windmatrix</p>
         <Link href='/ships' ><p className='text-white pt-1 text-lg'>Ships</p></Link>
       </div>
       <div className='pt-20'>
         <Chat
-          title="Icebreaker Chat"
+          title="Windmatrix Chat"
           bodyPlaceholder="Response will appear here..."
           inputPlaceholder="Type a message..."
           aiTag="portfolio-general-chat"
